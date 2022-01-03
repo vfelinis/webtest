43431f20106a181274f77c00e0cc4e18adc511df
ponomarev_mike <ponomarev.mike@sharp-dev.net>
2022-01-03 17:32:07 +0500

    init

---
+++ WebTest/Data/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebTest.Data.Models;
+
+namespace WebTest.Data
+{
+    public class ApplicationContext: DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("Users");
+        }
+    }
+}
---
+++ WebTest/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.EntityFrameworkCore;
+using WebTest.Data;
+using WebTest.Data.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationContext>(x => x.UseSqlite(connectionString));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", (ApplicationContext dbContext) =>
+{
+    return dbContext.Users.ToList();
+});
+app.MapPost("/", (string firstName, string lastName, ApplicationContext dbContext) =>
+{
+    var user = new User
+    {
+        FirstName = firstName,
+        LastName = lastName,
+        DateCreated = DateTimeOffset.Now,
+    };
+    dbContext.Users.Add(user);
+    dbContext.SaveChanges();
+    return Results.Ok();
+});
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationContext>();
+    dbContext.Database.Migrate();
+}
+
+app.Run();
